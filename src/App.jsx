1f1d51b94078bea34c43bc05be7f005dc06646fc
masyuko0222 <masyuko0222@gmail.com>
2023-11-08 20:34:43 +0900

    ✨ SWRを使ってLoading, Error時の画面表示を追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
+import useSWR from "swr";
 import "./App.css";
+
+const fetcher = async (params) => {
+  const [url, headers] = params;
+
+  const res = await fetch(url, { headers });
+
+  if (!res.ok) {
+    throw new Error("An error occurred while fetching the data.");
+  }
+  return res.json();
+};
 
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
 
-  const [status, setStatus] = useState("");
+  const { data, error, isLoading } = useSWR([url, headers], fetcher);
 
-  fetch(url, { headers })
-    .then((res) => res.json())
-    .then((json) => setStatus(json.description));
+  if (error) return <p>Failed to load.</p>;
+  if (isLoading) return <p>Loading...</p>;
 
-  return <>{status && <p>Status : {status}</p>}</>;
+  return <>{data && <p>Status : {data.description}</p>}</>;
 }
 
 export default App;
